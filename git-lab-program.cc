68693cc741bc2bb7be55827006a50388f00e47ed
Treyce Albin <ta642217@ohio.edu>
2019-05-17 14:25:44 -0400

    added git-lab-program.cc

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Treyce Albin
+ *        Date: 5/16/2019
+ * Description: Output "Hello Git!!""
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl; 
+    return 0;
+}// main
